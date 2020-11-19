3d869a8e64dd414a7500e2708d82648db75c46ef
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2020-11-19 07:41:16 -0300

    Aula14 - Parametros com Regras

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,28 @@ Route::get('/', function () {
 });
 
 //rota fixa
-Route::get('/rota_fixa/sejabemvindo', function() {
+Route::get('/aula12/rota_fixa/sejabemvindo', function() {
     return("Rota seja bem vindo");
 });
 
 //rota com parametro
-Route::get('/ola/{nome}/{sobrenome}', function($nome,$sobrenome) {
+Route::get('/aula12/ola/{nome}/{sobrenome}', function($nome,$sobrenome) {
     return("Olá $nome $sobrenome, seja Bem Vindo!");
 });
+
+// rota opcional
+/** utilizamos rota opcional quando o usuario não é obrigado informar o parametro
+ * na rota, assim podemos definir um valor padrão para o parametro em caso o usuário não informar
+ * Caso nenhum valor informado, retornará "Carlos Eduardo dos Santos Roberto" */
+Route::get('/aula13/{nome?}',function($nome="Carlos Eduardo dos Santos Roberto"){
+    return $nome;
+});
+
+// rota com regra
+Route::get('/aula14/rotacomregra/{qtd}/{nome}', function ($qtd,$nome) {
+    for($i=1;$i<=$qtd;$i++) {
+        echo "<p>$i - $nome</p>";
+    };
+})
+->where('nome','[A-Za-z]+')
+->where('qtd','[0-9]+');
