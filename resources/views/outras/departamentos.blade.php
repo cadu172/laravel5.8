86b2efea7f748a5ffc1065ce9823762115a553cf
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2020-12-17 18:52:08 -0300

    copia dos fontes para o mac, executando laravel com vagrabt

--- resources/views/outras/departamentos.blade.php
+++ resources/views/outras/departamentos.blade.php
@@ -11,9 +11,14 @@
         <li>Cobrança</li>
         <li>Administrativo</li>
     </ul>
-    <x-Alerta tipo="info" titulo="Titulo da minha Caixa de mensagem" mensagem="Deu erro aqui neste trecho de código" />
+    <x-Alerta 
+        tipo="info"
+        titulo="Titulo da minha Caixa de mensagem"
+        mensagem="Deu erro aqui neste trecho de código" />
+    
     <x-Alerta tipo="warning" titulo="Titulo da minha Caixa de mensagem" mensagem="Deu erro aqui neste trecho de código" />
     <x-Alerta tipo="success" titulo="Titulo da minha Caixa de mensagem" mensagem="Deu erro aqui neste trecho de código" />
     <x-Alerta tipo="error" titulo="Titulo da minha Caixa de mensagem" mensagem="Deu erro aqui neste trecho de código" />
+    <x-Alerta tipo="success" titulo="Aqui vai o titulo" mensagem="Aqui vai a descricao do erro" />
 
 @endsection('conteudo')
