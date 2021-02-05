f367d2d6af0f8d647e87ffd1011bf200958c8551
Carlos <cadu172@gmail.com>
2021-02-05 02:26:36 +0000

    Concluido até o capitulo 9 Eloquent ORM, aula 58 utlizando querys

--- database/migrations/2021_02_05_012335_create_departaments.php
+++ database/migrations/2021_02_05_012335_create_departaments.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProducts extends Migration
+class CreateDepartaments extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateProducts extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('departaments', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('stock');
-            $table->double('price');
-            $table->double('description')->nullable();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateProducts extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('departaments');
     }
 }
